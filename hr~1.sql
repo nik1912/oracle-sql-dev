05c63f4486e546813597ce3da2e3daf69c426836
nik1912 <nikolajsolovej10@gmail.com>
2024-08-08 23:33:27 +0300

    First commit4

--- hr~1.sql
+++ hr~1.sql
@@ -10,5 +10,4 @@ SELECT e.employee_id ,e.job_id,e.first_name, e.last_name, e.salary,
    FROM employees e
    
    
-   where 1=1
-   and e.job_id in ('FI_ACCOUNT','IT_PROG','PU_CLERK');
+   where e.job_id in ('FI_ACCOUNT','IT_PROG','PU_CLERK');
